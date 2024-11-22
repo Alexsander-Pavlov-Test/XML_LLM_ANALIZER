7faf43eb90feb78e6b4b991cd1ed0d4a9a2eed76
Alexsander-Pavlov-Test <soul.pam106@gmail.com>
2024-11-22 22:10:02 +0600

    get_head, get_list, get_generator in abstractxmlparser

--- parsers/base/abc.py
+++ parsers/base/abc.py
@@ -1,16 +1,27 @@
 from collections.abc import Sequence
 from abc import ABC, abstractmethod
-from typing import ClassVar
-from xml.dom.minidom import parse, parseString
+from typing import Any, ClassVar, TextIO, Generator, Callable
 
 
 class AbstractXMLParser(ABC, Sequence):
     """
     Абстрактный класс XML Парсера
     """
-    PARSER: ClassVar['parse' | 'parseString' | None] = None
+    PARSER: ClassVar[Callable[...] | None] = None
 
     @abstractmethod
     @classmethod
-    def get_parser(cls):
+    def get_parser(cls) -> Callable[...] | None:
+        pass
+
+    @abstractmethod
+    def get_head(self) -> dict[str, str]:
+        pass
+
+    @abstractmethod
+    def get_list(self) -> list[dict[str, str]] | None:
+        pass
+
+    @abstractmethod
+    def get_generator(self) -> Generator[dict[str, str], Any] | None:
         pass
--- parsers/base/base_xml.py
+++ parsers/base/base_xml.py
@@ -1,3 +1,5 @@
+from typing import Generator, TextIO, Any
+
 from .abc import AbstractXMLParser
 from . import BaseModelNotProvideError
 
@@ -9,16 +11,48 @@ class BaseXMLParser(AbstractXMLParser):
     PARSER = None
 
     def __init__(self,
-                 xml_string: str,
+                 xml: str | TextIO,
                  target_items: str,
                  head_name: str | None = None,
                  ) -> None:
-        pass
+        self.xml = xml
+        self._check_xml_instance(xml=xml)
+        self.target_items = target_items
+        self.head_name = head_name
 
     @classmethod
     def get_parser(cls):
+        """
+        Получение текущего парсера
+        """
         parser = cls.PARSER
         if not parser:
             raise BaseModelNotProvideError(
-                'Вы не можете использовать базовый класс')
+                'Вы не можете использовать базовый класс',
+                )
         return parser
+
+    def _check_xml_instance(self, xml: str | TextIO) -> None:
+        cls = type(self).__name__
+        raise BaseModelNotProvideError(
+                f'Вы не можете использовать базовый класс {cls}',
+                )
+
+    def _parse(self,
+               xml,
+               target_items,
+               ) -> list[dict[str, str]] | None:
+        """
+        Метод парсинга данных из XML
+        """
+        parser = self.get_parser()
+
+    def get_list(self) -> list[dict[str, str]] | None:
+        """
+        Возвращает список после парсинга
+        """
+    
+    def get_generator(self) -> Generator[dict[str, str], Any] | None:
+        """
+        Возвращает генератор после парсинга
+        """
